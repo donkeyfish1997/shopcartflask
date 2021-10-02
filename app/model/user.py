9c9aa1a5df5147f38210c10e163cb6b18b718856
donkeyfish1997 <donkeyfish1997@gamil.com>
2021-10-02 15:33:03 +0800

    make it better

--- app/model/user.py
+++ app/model/user.py
@@ -3,7 +3,7 @@ from .. import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
-    pic=db.Column(db.LargeBinary)
+    pic=db.Column(db.TEXT)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)

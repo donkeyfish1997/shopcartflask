9fd8eb463e435384f86e1486b590d80a41023ead
donkeyfish1997 <donkeyfish1997@gamil.com>
2021-10-02 12:53:15 +0800

    ..

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ CORS(app, support_credentials=True)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pass@127.0.0.1:3306/jobProject'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b4937878ebe4b6:0080f4bb@us-cdbr-east-04.cleardb.com/heroku_41292c346ecbd3a'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SESSION_COOKIE_SAMESITE']='None'
+app.config['SESSION_COOKIE_SECURE']='True'
 db = SQLAlchemy(app) 
 
 app.config['SECRET_KEY'] = 'test'
